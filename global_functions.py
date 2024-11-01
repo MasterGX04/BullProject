73a01d6082f1b2e1722b235e71ed73cfa0491056
MasterGX04 <47278400+MasterGX04@users.noreply.github.com>
2024-10-31 21:03:18 -0400

    Update and rename grid_size.py to global_functions.py

---
+++ global_functions.py
@@ -0,0 +1,8 @@
+GRID_SIZE = 13
+ROBOT_MOVES = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, -1), (1, -1), (-1, 1)]
+
+# Possible movements: up, down, left, and right
+BULL_MOVES = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+def manhattanDistance(pos1, pos2):
+    return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
