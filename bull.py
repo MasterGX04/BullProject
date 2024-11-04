c6c9022669f82100b2f9fe32e9ec92fb38787d95
TeaGek <159391085+TeaGek@users.noreply.github.com>
2024-11-04 12:24:02 -0500

    Update bull.py

--- bull.py
+++ bull.py
@@ -1,5 +1,5 @@
 import random
-from grid_size import GRID_SIZE
+from global_functions import GRID_SIZE
 
 # Possible movements: up, down, left, and right
 BULL_MOVES = [(0, 1), (0, -1), (1, 0), (-1, 0)]
@@ -19,38 +19,43 @@ def isWithin5x5Square(bullPosition, robotPosition):
 
 # Bull movement logic
 def moveBull(bullPosition, robotPosition, obstacles, corralPositions):
-    bullX, bullY = list(bullPosition)  # Convert tuple to list for mutation
+    bullX, bullY = bullPosition
     possibleMoves = []
-    
+    global corralPos
+    corralPos = corralPositions
+
     # If robot is within a 5x5 square around bull
     if isWithin5x5Square(bullPosition, robotPosition):
         for move in BULL_MOVES:
             newBullX = bullX + move[0]
             newBullY = bullY + move[1]
             newPosition = [newBullX, newBullY]
-            # Ensure bull moves towards the robot
-            if (0 <= newBullX < GRID_SIZE and 0 <= newBullY < GRID_SIZE) and \
-               (newBullX, newBullY) not in obstacles and newPosition != list(robotPosition):
+            # Ensure bull moves to robot
+            if (0 <= newBullX < GRID_SIZE and 0 <= newBullY < GRID_SIZE) and (
+            newBullX, newBullY) not in obstacles and newPosition != robotPosition:
                 # Check if this move maintains or decreases the distance to the robot
                 if manhattanDistance(newPosition, robotPosition) <= manhattanDistance(bullPosition, robotPosition):
                     possibleMoves.append(move)
+        # end bull move check
         # Randomly choose valid move for bull
         if possibleMoves:
             chosenMove = random.choice(possibleMoves)
-            bullX += chosenMove[0]
-            bullY += chosenMove[1]
+            if chosenMove == robotPosition:
+                return
+            else:
+                bullPosition[0] += chosenMove[0]
+                bullPosition[1] += chosenMove[1]
     else:
         # Random move if robot is outside 5x5 area
         for move in BULL_MOVES:
             newBullX = bullX + move[0]
             newBullY = bullY + move[1]
-            if (0 <= newBullX < GRID_SIZE and 0 <= newBullY < GRID_SIZE and
-                    (newBullX, newBullY) != tuple(robotPosition) and
-                    (newBullX, newBullY) not in obstacles):
+            # Grid border check and not going to same position as obstacle or robot
+            if 0 <= newBullX < GRID_SIZE and 0 <= newBullY < GRID_SIZE and (newBullX, newBullY) != tuple(
+                    robotPosition) and (newBullX, newBullY) not in obstacles:
                 possibleMoves.append(move)
         if possibleMoves:
             move = random.choice(possibleMoves)
-            bullX += move[0]
-            bullY += move[1]
-
-    return (bullX, bullY)  # Return the new position as a tuple
+            bullPosition[0] += move[0]
+            bullPosition[1] += move[1]
+# End moveBull
